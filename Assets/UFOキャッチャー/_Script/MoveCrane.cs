73c0d644d894453f3d629c852aee79ec76179bdf
kiritan <kirinoshitano@gmail.com>
2020-12-12 14:02:23 +0900

    初回のコミット

---
+++ Assets/UFOキャッチャー/_Script/MoveCrane.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCrane : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+      Transform tr = this.transform;   
+      int i ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+            
+    }
+}
